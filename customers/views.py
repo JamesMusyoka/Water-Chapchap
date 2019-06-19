c05c16d2a92b2313681ab3c40ee31f20d6608a0e
john victor <johnvictor0002@gmail.com>
2019-06-19 13:42:22 +0300

    addedcfunction that renders the frequently asked questions page

--- customers/views.py
+++ customers/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+    
+def faqs(request):
+    return render(request,'faqs.html')
